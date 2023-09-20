fecd6d6086043cdba7444a0376bfd8bf21f12979
Daniel Wolf <daniel-wolf@outlook.com>
2023-09-20 14:04:25 +0200

    update

--- lib/domain/core/value_object/value_object.dart
+++ lib/domain/core/value_object/value_object.dart
@@ -3,25 +3,21 @@ import 'package:bloc_input_valueobject/domain/core/errors/failures.dart';
 import 'package:dartz/dartz.dart';
 import 'package:equatable/equatable.dart';
 
-import '../common/common_interfaces.dart';
+import '../common/interfaces.dart';
 
-/// Serves as a base class for custom value object implementations.
-///
-/// This is a modified version of "Fred Grott" (https://github.com/fredgrott)
-/// & "Matej Rešetár" (https://github.com/ResoCoder/flutter-tdd-clean-architecture-course).
 class ValueObject<T> extends Equatable implements Validatable {
   /// Holds either a failure (left / l) or a valid value (right / r) of type [T].
   ///
   /// Usage:
   /// ```dart
-  /// final vo = ValueObject<Failure, String>(right('Hello!'));
-  /// vo.value.fold((l) => print(l), (r) => print(r)); // prints "Hello!"
+  /// final v = ValueObject<Failure, String>(right('Hello!'));
+  /// v.value.fold((l) => print(l), (r) => print(r)); // prints "Hello!"
   /// ```
   final Either<Failure, T> value;
 
   const ValueObject(this.value);
 
-  /// Gets a valid value of type [T] or throws an error.
+  /// Gets a valid value of type [T] or throws an [ValueObjectException].
   T getOrThrow() => value.fold(
       (l) => throw ValueObjectException(message: l.toString()), (r) => r);
 
