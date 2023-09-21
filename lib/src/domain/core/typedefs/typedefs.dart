97ef35a1f12b5b86c4f8415351961ddfac5862d3
Daniel Wolf <daniel-wolf@outlook.com>
2023-09-21 14:14:56 +0200

    update

--- lib/src/domain/core/typedefs/typedefs.dart
+++ lib/src/domain/core/typedefs/typedefs.dart
@@ -1,6 +1,5 @@
 import 'package:bloc_input_valueobject/src/domain/core/errors/failures.dart';
 import 'package:dartz/dartz.dart';
 
-typedef Result<T> = Either<Failure, T>;
 typedef ResultFuture<T> = Future<Either<Failure, T>>;
 typedef DataMap = Map<String, dynamic>;
--- lib/src/domain/settings/usecases/exist_settings.dart
+++ lib/src/domain/settings/usecases/exist_settings.dart
@@ -1,5 +1,5 @@
 import 'package:bloc_input_valueobject/src/domain/core/typedefs/typedefs.dart';
-import 'package:bloc_input_valueobject/src/domain/core/usecase/use_case.dart';
+import 'package:bloc_input_valueobject/src/domain/core/usecase/usecase.dart';
 import 'package:bloc_input_valueobject/src/domain/settings/repositories/settings_repository.dart';
 
 class ExistSettings implements UsecaseWithoutParams<bool> {
